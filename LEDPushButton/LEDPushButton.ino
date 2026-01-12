b6ae7aeac949aee026ce8665ec87568d273715e9
JM Bell <justinmbell1997@gmail.com>
2026-01-12 09:41:04 -0600

    LEDPushButton - completed but still needs to be tested

---
+++ LEDPushButton/LEDPushButton.ino
@@ -0,0 +1,22 @@
+#define BUTTON_PIN 2
+#define LED_PIN 11
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+/**
+*
+* @brief This method turns on LED if the push button is pressed.
+*/
+void loop() {
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  delay(50);
+}
