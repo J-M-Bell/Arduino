7695454dab40798edefb2f0403e363930b461bd7
JM Bell <justinmbell1997@gmail.com>
2026-01-12 11:02:55 -0600

    LEDPotentiometer - added println

--- LEDPotentiometer/LEDPotentiometer.ino
+++ LEDPotentiometer/LEDPotentiometer.ino
@@ -8,6 +8,7 @@ void setup() {
 
 void loop() {
   int potentiometer = analogRead(POTENTIOMETER_PIN);
+  Serial.println(potentiometer);
   float scaled_potentiometer = potentiometer / 4;
   analogWrite(LED_PIN, scaled_potentiometer);
 }
