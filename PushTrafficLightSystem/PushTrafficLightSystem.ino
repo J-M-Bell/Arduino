9deccb69622284b0fe661579192b74de9f25ab4b
JM Bell <justinmbell1997@gmail.com>
2026-01-12 13:12:24 -0600

    PushTrafficLightSystem - updated solution

--- PushTrafficLightSystem/PushTrafficLightSystem.ino
+++ PushTrafficLightSystem/PushTrafficLightSystem.ino
@@ -1,7 +1,11 @@
 #define BUTTON_PIN 2
+
 #define RED_PIN 12
 #define YELLOW_PIN 11
 #define GREEN_PIN 10
+
+int LEDBlinkState = 1;
+
 void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(YELLOW_PIN, OUTPUT);
@@ -11,19 +15,20 @@ void setup() {
 
 void loop() {
   if (digitalRead(BUTTON_PIN) == LOW) {
-    // red and green on, yellow off
-    digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(GREEN_PIN, HIGH);
+    if (LEDBlinkState == 1) {
+      // red and green on, yellow off
+      digitalWrite(YELLOW_PIN, LOW);
+      digitalWrite(RED_PIN, HIGH);
+      digitalWrite(GREEN_PIN, HIGH);
+      LEDBlinkState = 2;
+    }
+    else {
+      // red and green off, yellow on
+      digitalWrite(RED_PIN, LOW);
+      digitalWrite(GREEN_PIN, LOW);
+      digitalWrite(YELLOW_PIN, HIGH);
+      LEDBlinkState = 1;
+    }
     delay(300);
-
-    // red and green off, yellow on
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(YELLOW_PIN, HIGH);
-    delay(300);
-  }
-  else {
-    delay(1);
   }
 }
