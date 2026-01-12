c623e4556c826455cf55a44253512e3e2d2df302
JM Bell <justinmbell1997@gmail.com>
2026-01-12 12:31:00 -0600

    PushTrafficLightSystem - created program will test later

---
+++ PushTrafficLightSystem/PushTrafficLightSystem.ino
@@ -0,0 +1,29 @@
+#define BUTTON_PIN 2
+#define RED_PIN 12
+#define YELLOW_PIN 11
+#define GREEN_PIN 10
+void setup() {
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    // red and green on, yellow off
+    digitalWrite(YELLOW_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN, HIGH);
+    delay(300);
+
+    // red and green off, yellow on
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(YELLOW_PIN, HIGH);
+    delay(300);
+  }
+  else {
+    delay(1);
+  }
+}
