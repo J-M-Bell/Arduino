92a0ea623db7b66cf750e3a926641c9ce575f09b
JM Bell <justinmbell1997@gmail.com>
2026-01-12 09:39:29 -0600

    LEDFade - completed program; need to test with real circuit

--- LEDFade/LEDFade.ino
+++ LEDFade/LEDFade.ino
@@ -1,21 +1,35 @@
 #define LED_PIN 11
 
-
+/**
+*
+* @brief This sets up serial monitor and pins.
+*/
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
 }
 
-void writeToAnalogPin(int percentage){
-  analogWrite(LED_PIN, i);
+/**
+* @brief This method write to an Arduino analog pin 
+* to fade LED pin.
+* 
+* @param int pwm: the pulse width modulation value
+*/
+void writeToAnalogPin(int pwm){
+  analogWrite(LED_PIN, pwm);
   delay(5);
 }
 
+/**
+*
+* @brief This method fades LED in and out continuously.
+*/
 void loop() {
   // Fade LED from on to off using PWM
-  for (int i = 0; i <= 255; i++){
+  for (int i = 0; i <= 255; i++) {
     writeToAnalogPin(i);
   }
-  for (int i = 255; i >= 0; i--){
+  for (int i = 255; i >= 0; i--) {
     writeToAnalogPin(i);
   }
 }
