9000d2041576b65efa27bb9252a85c587de6cdf5
JM Bell <justinmbell1997@gmail.com>
2026-01-12 10:16:32 -0600

    LEDPotentiometer - created Potentiometer program

---
+++ LEDPotentiometer/LEDPotentiometer.ino
@@ -0,0 +1,13 @@
+#define POTENTIOMETER_PIN A2
+#define LED_PIN 11
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  int potentiometer = analogRead(POTENTIOMETER_PIN);
+  float scaled_potentiometer = potentiometer / 4;
+  analogWrite(LED_PIN, scaled_potentiometer);
+}
