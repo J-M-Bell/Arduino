02d647ccd3eb0bf37b14201c0b31ef8a55e6c5d9
JM Bell <justinmbell1997@gmail.com>
2026-01-13 10:24:04 -0600

    CircuitTimer2 - created CircuitTimer2 program

---
+++ CircuitTimer2/CircuitTimer2.ino
@@ -0,0 +1,75 @@
+#define RED_PIN 12
+#define YELLOW_PIN 11
+#define GREEN_PIN 10
+
+#define POTENTIOMETER_PIN A2
+#define BUTTON_PIN 2
+
+#define INPUT_PIN_COUNT 3
+#define OUTPUT_PIN_COUNT 2
+
+
+unsigned long timeCounter = millis();
+unsigned long timeIntervalSerialInput = 100;
+
+int LEDState = LOW;
+
+byte inputPins[INPUT_PIN_COUNT] = {RED_PIN, YELLOW_PIN, GREEN_PIN};
+byte outputPins[OUTPUT_PIN_COUNT] = {POTENTIOMETER_PIN, BUTTON_PIN};
+
+void setInputPins() {
+  for (byte inputPin : inputPins) {
+    pinMode(inputPin, OUTPUT);
+    pinMode(inputPin, LOW);
+  }
+}
+
+void setOutputPins() {
+  for (byte outputPin : outputPins) {
+    pinMode(outputPin, INPUT);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  setInputPins();
+  setOutputPins();
+}
+
+void loop() {
+  unsigned long timeNow = millis();
+  
+  // 1. get delay time from user if available
+  if (Serial.available() > 0) {
+    int data = Serial.parseInt();
+    if (data >= 100 && data <= 1000) {
+      timeIntervalSerialInput = data;
+    }
+  }
+  // 2. add delay time specified by the user
+  if (timeNow - timeCounter > timeIntervalSerialInput) {
+    if (LEDState == LOW) {
+      LEDState = HIGH;
+    }
+    else {
+      LEDState = LOW;
+    }
+    digitalWrite(RED_PIN, LEDState);
+    timeCounter += timeIntervalSerialInput;
+  }
+
+  // 3. set LED 2 (pin 11) brightness with potentiometer
+  int potentiometer = analogRead(POTENTIOMETER_PIN);
+  int pwm = potentiometer / 4;
+  analogWrite(YELLOW_PIN, pwm); 
+  
+
+  // 4. power on LED 3 when button is pressed
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(GREEN_PIN, HIGH);
+  }
+  else {
+    digitalWrite(GREEN_PIN, LOW);
+  }
+}
