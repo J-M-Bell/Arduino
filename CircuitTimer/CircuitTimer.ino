63b48ff9fe3b09b333b5f8aee3a8b5525574e403
JM Bell <justinmbell1997@gmail.com>
2026-01-13 09:31:51 -0600

    CircuitTimer - created LED timer program

---
+++ CircuitTimer/CircuitTimer.ino
@@ -0,0 +1,34 @@
+#define RED_PIN 12
+
+unsigned long previousTimeSerialInput = millis();
+unsigned long timeIntervalSerialInput = 100;
+int LEDState = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(RED_PIN, LOW);
+
+}
+
+void loop() {
+  unsigned long timeNow = millis();
+  
+  if (Serial.available() > 0) {
+    int data = Serial.parseInt();
+    if (data >= 100 && data <= 1000) {
+      timeIntervalSerialInput = data;
+    }
+  }
+  if (timeNow - previousTimeSerialInput > timeIntervalSerialInput) {
+    if (LEDState == LOW) {
+      LEDState = HIGH;
+    }
+    else {
+      LEDState = LOW;
+    }
+    digitalWrite(RED_PIN, LEDState);
+    previousTimeSerialInput += timeIntervalSerialInput;
+  }
+}
