1005dd2806a4ca0da2a75fe2d03e6943c447d84d
JM Bell <justinmbell1997@gmail.com>
2026-01-16 07:38:10 -0600

    LEDSerialCommunication - Created documentation for LEDSerialCommunication program

--- LEDSerialCommunication/LEDSerialCommunication.ino
+++ LEDSerialCommunication/LEDSerialCommunication.ino
@@ -1,8 +1,13 @@
 #define RED_PIN 12
 
+// LED state and delay variables
 int blinkDelay = 100;
 int LEDState = LOW;
 
+/**
+ * @brief Sets up serial and pins.
+ * 
+ */
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(10);
@@ -11,14 +16,22 @@ void setup() {
 
 }
 
+/**
+ * @brief This program blinks the LED depending on the blink delay
+ * value from the user if it is between 100 and 1000.
+ * 
+ */
 void loop() {
-  if (Serial.available() > 0) {
+
+  if (Serial.available() > 0) { // gets input from serial
     int data = Serial.parseInt();
     if (data >= 100 && data <= 1000) {
       blinkDelay = data;
     }
   }
-  if (LEDState == LOW) {
+
+  // blinks LED depending on delay input from user
+  if (LEDState == LOW) { 
     LEDState = HIGH;
   }
   else {
