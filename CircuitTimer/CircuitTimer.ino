0c2cd7317fec22772c9e849c81cd538875c75606
JM Bell <justinmbell1997@gmail.com>
2026-01-16 07:32:20 -0600

    CircuitTimer - added documentation to CircuitTimer program

--- CircuitTimer/CircuitTimer.ino
+++ CircuitTimer/CircuitTimer.ino
@@ -1,8 +1,10 @@
 #define RED_PIN 12
 
+// timer delay variables
 unsigned long timeCounter = millis();
 unsigned long timeIntervalSerialInput = 100;
-int LEDState = LOW;
+
+int LEDState = LOW; // LED blinking variable
 
 void setup() {
   Serial.begin(9600);
@@ -12,16 +14,23 @@ void setup() {
 
 }
 
+/**
+ * @brief This program takes input from the user to determine the
+ * rate that the LED will blink.
+ * 
+ */
 void loop() {
   unsigned long timeNow = millis();
   
-  if (Serial.available() > 0) {
+  
+  if (Serial.available() > 0) { // set blink rate depending on the user input
     int data = Serial.parseInt();
     if (data >= 100 && data <= 1000) {
       timeIntervalSerialInput = data;
     }
   }
-  if (timeNow - timeCounter > timeIntervalSerialInput) {
+
+  if (timeNow - timeCounter > timeIntervalSerialInput) { // blink LED after a delay
     if (LEDState == LOW) {
       LEDState = HIGH;
     }
