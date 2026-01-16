2f85e82b5fcfdc9a64b428a5a7c471e665b08705
JM Bell <justinmbell1997@gmail.com>
2026-01-16 07:39:10 -0600

    PushTrafficLightSystem - created documentation for PushTrafficLightSystem program

--- PushTrafficLightSystem/PushTrafficLightSystem.ino
+++ PushTrafficLightSystem/PushTrafficLightSystem.ino
@@ -4,21 +4,32 @@
 #define YELLOW_PIN 11
 #define GREEN_PIN 10
 
+// LED state variables
 #define LED_PIN_COUNT 3
-
 int LEDBlinkState = 1;
-
 byte LEDPins[LED_PIN_COUNT] = {RED_PIN, YELLOW_PIN, GREEN_PIN};
 
+/**
+ * @brief Sets up LEDs
+ * 
+ */
 void setup() {
-  setLEDPins();
+  setPins();
 }
 
+/**
+ * @brief This program toggles the LED from on to off.
+ * 
+ */
 void loop() {
   toggleLEDState();
 }
 
-void setLEDPins() {
+/**
+ * @brief This method sets up the pins. 
+ * 
+ */
+void setPins() {
   pinMode(BUTTON_PIN, INPUT);
 
   for (byte pin : LEDPins) {
@@ -27,17 +38,20 @@ void setLEDPins() {
   }
 }
 
+/**
+ * @brief This method toggles between the red and green LEDs and the yellow LED if the button
+ * is released.
+ * 
+ */
 void toggleLEDState() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    if (LEDBlinkState == 1) {
-      // red and green on, yellow off
+  if (digitalRead(BUTTON_PIN) == LOW) { // if button is not pressed
+    if (LEDBlinkState == 1) { // red and green on, yellow off
       digitalWrite(YELLOW_PIN, LOW);
       digitalWrite(RED_PIN, HIGH);
       digitalWrite(GREEN_PIN, HIGH);
       LEDBlinkState = 2;
     }
-    else {
-      // red and green off, yellow on
+    else { // red and green off, yellow on
       digitalWrite(RED_PIN, LOW);
       digitalWrite(GREEN_PIN, LOW);
       digitalWrite(YELLOW_PIN, HIGH);
