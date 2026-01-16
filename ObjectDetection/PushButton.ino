a7552e79fb82de5d83b5af1475b706f3a43a0605
JM Bell <justinmbell1997@gmail.com>
2026-01-16 13:34:36 -0600

    PushButton - created PushButton file for ObjectDetection project

---
+++ ObjectDetection/PushButton.ino
@@ -0,0 +1,20 @@
+#define BUTTON_PIN 2
+
+/**
+ * @brief Sets up the Push Button component
+ * 
+ */
+void setUpPushButton() {
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+/**
+ * @brief This method unlocks the bot if the
+ * push button is pressed.
+ * 
+ */
+void unlockBot() {
+  // call unlockLEDs()
+
+  // call printDistanceInCentimetersScreen() 
+}
