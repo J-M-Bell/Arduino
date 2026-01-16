d2f1380d31d1553ff80cc35e4d65dac458f6c169
JM Bell <justinmbell1997@gmail.com>
2026-01-16 07:38:38 -0600

    Photoresistor - created documentation for Photoresistor program

--- Photoresistor/Photoresistor.ino
+++ Photoresistor/Photoresistor.ino
@@ -4,20 +4,30 @@
 #define GREEN_PIN 10
 
 #define LUMINOSITY_THRESHOLD 300
+
+/**
+ * @brief Sets up serial.
+ * 
+ */
 void setup() {
   Serial.begin(9600);
 }
 
+/**
+ * @brief Thsi program 
+ * 
+ */
 void loop() {
   int luminosity = analogRead(PHOTORESISTOR_PIN);
 
-  if (luminosity < LUMINOSITY_THRESHOLD) {
+  if (luminosity < LUMINOSITY_THRESHOLD) { // powers on Red LED depending on luminosity threshold
     digitalWrite(RED_PIN, LOW);
   }
   else {
     digitalWrite(RED_PIN, HIGH);
   }
-  
+
+  // sets brightness of Green LED depending on how bright the room is
   int scaled = 255 - (luminosity / 4);
   analogWrite(GREEN_PIN, scaled);
 }
