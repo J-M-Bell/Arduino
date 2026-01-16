a7d899279b13df5f41f0c6e93d42a8240eea7582
JM Bell <justinmbell1997@gmail.com>
2026-01-16 07:37:14 -0600

    LEDPotentiometer - Created documentation for LEDPotentiometer program

--- LEDPotentiometer/LEDPotentiometer.ino
+++ LEDPotentiometer/LEDPotentiometer.ino
@@ -1,11 +1,19 @@
 #define POTENTIOMETER_PIN A2
 #define LED_PIN 11
 
+/**
+ * @brief Set up serial and pin.
+ * 
+ */
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
 }
 
+/**
+ * @brief This program fades LED depending on the reading from the potentimeter.
+ * 
+ */
 void loop() {
   int potentiometer = analogRead(POTENTIOMETER_PIN);
   Serial.println(potentiometer);
