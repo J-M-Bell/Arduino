de2179420cf9437f4930405ec049612e9c2b7ffd
JM Bell <justinmbell1997@gmail.com>
2026-01-20 07:31:42 -0600

    UltrasonicSensor (Final) - editted lockBot function

--- ObjectDetection/UltrasonicSensor.ino
+++ ObjectDetection/UltrasonicSensor.ino
@@ -91,8 +91,9 @@ double getDistanceinInches() {
  */
 void lockBot(double distance) {
   // call 'lockLED' function
-
+  
   // call 'printErrorMessageScreen' function
+  printErrorMessageScreen();
 }
 
 /**
