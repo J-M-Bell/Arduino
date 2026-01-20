411fb518855cc27796d1e0d1c77ff48e69d59046
JM Bell <justinmbell1997@gmail.com>
2026-01-20 10:13:53 -0600

    DebounceButton - refactored debounce button program

--- DebounceButton/DebounceButton.ino
+++ DebounceButton/DebounceButton.ino
@@ -44,6 +44,42 @@ void setInputPins() {
 }
 
 /**
+ * @brief This method blinks the RED LED.
+ * 
+ */
+void blinkLED() {
+  if (LEDBlinkState == LOW) {
+      LEDBlinkState = HIGH;
+    }
+    else {
+      LEDBlinkState = LOW;
+    }
+    digitalWrite(RED_PIN, LEDBlinkState);
+    LEDClock += LEDTimeDelay;
+}
+
+/**
+ * @brief This method is for toggle between the green pin and yellow pin
+ * being powered on.
+ * 
+ */
+void toggleLEDs() {
+  if (buttonState == HIGH) {
+    if (LEDToggleState == 1){
+      LEDToggleState = 2;
+      digitalWrite(YELLOW_PIN, HIGH);
+      digitalWrite(GREEN_PIN, LOW);
+      
+    }
+    else {
+      LEDToggleState = 1;
+      digitalWrite(YELLOW_PIN, LOW);
+      digitalWrite(GREEN_PIN, HIGH);
+    }
+  }
+}
+
+/**
  * @brief Set up serial and pins
  * 
  */
@@ -64,17 +100,9 @@ void loop() {
 
   // make LED 1 blink every second
   if (timeNow - LEDClock > LEDTimeDelay) {
-    if (LEDBlinkState == LOW) {
-      LEDBlinkState = HIGH;
-    }
-    else {
-      LEDBlinkState = LOW;
-    }
-    digitalWrite(RED_PIN, LEDBlinkState);
-    LEDClock += LEDTimeDelay;
+    blinkLED();
     //Serial.println("in LED loop.");
   }
-  
   
   // pressed: (LOW -> HIGH): toggle LED 2 & 3
   // toggle LEDs: power on 2, power off 3
@@ -84,19 +112,6 @@ void loop() {
       clickTimeCounter = timeNow;
       buttonState = newButtonState;
     }
-    if (buttonState == HIGH) {
-      if (LEDToggleState == 1){
-        LEDToggleState = 2;
-        digitalWrite(YELLOW_PIN, HIGH);
-        digitalWrite(GREEN_PIN, LOW);
-        
-      }
-      else {
-        LEDToggleState = 1;
-        digitalWrite(YELLOW_PIN, LOW);
-        digitalWrite(GREEN_PIN, HIGH);
-        
-      }
-    }
+    toggleLEDs();
   }
 }
