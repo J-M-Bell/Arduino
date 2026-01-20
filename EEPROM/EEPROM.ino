c591a0828b4784d7cd4f2924ce935131d869a9bd
JM Bell <justinmbell1997@gmail.com>
2026-01-20 10:29:16 -0600

    EEPROM - refactored EEPROM program

--- EEPROM/EEPROM.ino
+++ EEPROM/EEPROM.ino
@@ -9,12 +9,38 @@
 //int maxIntensity = 255;
 byte maxIntensity;
 
+void setMaxIntensity() {
+  if (Serial.available() > 0) { // get max intensity from serial
+    int data = Serial.parseInt();
+    if (data >= 1 && data <= 255) {
+      maxIntensity = data;
+
+      // save max intensity to EEPROM
+      Serial.print("The max intensity is now: ");
+      Serial.println(maxIntensity);
+      EEPROM.write(MAX_BRIGHTNESS_ADDRESS, maxIntensity);
+
+      // read value from EEPROM (debugging)
+      int memoryValue = EEPROM.read(MAX_BRIGHTNESS_ADDRESS);
+      Serial.print("The value saved is: ");
+      Serial.println(memoryValue);
+    }
+  }
+}
+
+void fadeLED() {
+  // LED on Pin 10 - fade in/out from potentiometer
+  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+  int scale = 1024 / maxIntensity;
+  int intensity = potentiometerValue / scale;
+  analogWrite(GREEN_PIN, intensity);
+}
 /**
  * @brief Sets up serial, pins, and max brightness from EEPROM.
  * 
  */
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.setTimeout(10);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(GREEN_PIN, LOW);
@@ -35,26 +61,6 @@ void setup() {
  * 
  */
 void loop() {
-  if (Serial.available() > 0) { // get max intensity from serial
-    int data = Serial.parseInt();
-    if (data >= 1 && data <= 255) {
-      maxIntensity = data;
-
-      // save max intensity to EEPROM
-      Serial.print("The max intensity is now: ");
-      Serial.println(maxIntensity);
-      EEPROM.write(MAX_BRIGHTNESS_ADDRESS, maxIntensity);
-
-      // read value from EEPROM (debugging)
-      int memoryValue = EEPROM.read(MAX_BRIGHTNESS_ADDRESS);
-      Serial.print("The value saved is: ");
-      Serial.println(memoryValue);
-    }
-  }
-
-  // LED on Pin 10 - fade in/out from potentiometer
-  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
-  int scale = 1024 / maxIntensity;
-  int intensity = potentiometerValue / scale;
-  analogWrite(GREEN_PIN, intensity);
+  setMaxIntensity();
+  fadeLED();  
 }
